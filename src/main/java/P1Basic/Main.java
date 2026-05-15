786d7cf6e24316e449066b4de3f8b1fc5ca3c9d4
PasikMacOs <frr.b006@gmail.com>
2026-05-15 15:37:29 +0500

    add P1Basic pacege and Main, Variables classes

---
+++ src/main/java/P1Basic/Main.java
@@ -0,0 +1,7 @@
+package P1Basic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("here we start");
+    }
+}
---
+++ src/main/java/P1Basic/Variables.java
@@ -0,0 +1,20 @@
+package P1Basic;
+
+public class Variables {
+    public static void main(String[] args) {
+        int MyInt = 577;
+        short MyShort = 233;
+        byte MyByte = 100; // 124 коментрии два сплеша много коментири так /* открывешь */ закрываешь
+        long MyLong = 1111L;
+
+        double MyDouble = 2.2;
+        float  MyFloat = 2.2f;
+
+        char MyChar = '2';
+        boolean MyBoolean = true;
+
+        System.out.println(MyInt + MyShort + MyByte + MyLong + MyDouble + MyFloat + MyChar );
+
+        System.out.println(MyBoolean);
+    }
+}
