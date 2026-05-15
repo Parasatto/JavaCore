1a0469c551804dead5f3fdb0ac08c272143ca9d8
PasikMacOs <frr.b006@gmail.com>
2026-05-15 17:18:41 +0500

    add For, IfElse, Scanner, While classes

---
+++ src/main/java/P1Basic/For.java
@@ -0,0 +1,10 @@
+package P1Basic;
+
+public class For {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("It`s a loop for: "+ i);
+        }
+    }
+}
---
+++ src/main/java/P1Basic/While.java
@@ -0,0 +1,13 @@
+package P1Basic;
+
+public class While {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num < 5){
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
