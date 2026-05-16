b8c9e83bf28783df02260d97fcf8bffdd50c9e11
PasikLinux <frr.b006@gmail.com>
2026-05-16 14:35:17 +0500

    add classes and some excec

---
+++ src/main/java/P1Basic/DoWhile.java
@@ -0,0 +1,12 @@
+package P1Basic;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int num = 0;
+
+        do {
+            System.out.println(num);
+            num++;
+        } while (num < 6);
+    }
+}
