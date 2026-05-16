9d4c786f0e0429db97f585a1eb727cfd61ee626f
Pasik <frr.b006gmail.com>
2026-05-16 21:05:24 +0500

    add arrays

---
+++ src/main/java/P1Basic/StringArrays.java
@@ -0,0 +1,23 @@
+package P1Basic;
+
+public class StringArrays {
+    public static void main(String[] args) {
+        String[] strings = new String[3];
+
+        strings[0]= "Pasik";
+        strings[1] = "Aza";
+        strings[2] = "Zenin";
+
+        for(String ss: strings){
+            System.out.println(ss + " добро пожаловать на вечеринку!");
+        }
+
+        int[] ints = {100, 1231, 233};
+        int sum = 0;
+        for(int i : ints){
+          sum = sum + i;
+        }
+
+        System.out.println(sum);
+    }
+}
