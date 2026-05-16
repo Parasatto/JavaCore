ac36bb1c636d4b5aedd6c6f61128aa39897cf17f
PasikMacOs <frr.b006@gmail.com>
2026-05-16 16:38:47 +0500

    add first array and switch

---
+++ src/main/java/P1Basic/Arrays.java
@@ -0,0 +1,23 @@
+package P1Basic;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+
+        for (int i = 0; i<numbers.length; i++){
+            numbers[i] = i;
+        }
+
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+
+        int[] nums = {1, 3, 5, 7, 9};
+
+        System.out.println("it`s a second array");
+        for (int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
