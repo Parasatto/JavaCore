4962d72114caf449b5e266ec5924062e96998522
PasikMacOs <frr.b006@gmail.com>
2026-05-20 15:25:34 +0500

    add work with constructor and static method&variable

--- src/main/java/P1Basic/ClassAndObject.java
+++ src/main/java/P1Basic/ClassAndObject.java
@@ -20,6 +20,10 @@ public class ClassAndObject {
 
 
         p1.speak();
+
+        System.out.println(Car.speed + " this is the car classes variable");
+        int res = Car.get2xspeed(200);
+        System.out.println(res + " this is the result of the static method class");
     }
 }
 
@@ -61,5 +65,48 @@ class Person{
 
     void sayHello(){
         System.out.println("Hi");
+    }
+}
+
+class Car {
+
+    private String name;
+    private int year;
+    private String country;
+
+    public static int speed = 200;
+
+    public Car(String name, int year, String country) {
+        this.name = name;
+        this.year = year;
+        this.country = country;
+    }
+
+    public static int get2xspeed(int hpower){
+        return hpower * speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
