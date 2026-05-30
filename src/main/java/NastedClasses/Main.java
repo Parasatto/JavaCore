702aa8618439fe9fa6089496b1cdf3c9e63137db
PasikMacOs <frr.b006@gmail.com>
2026-05-30 14:33:28 +0500

    work with nasted Class

---
+++ src/main/java/NastedClasses/Main.java
@@ -0,0 +1,14 @@
+package NastedClasses;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Computer c1 = new Computer("model one");
+
+        c1.performSelfTest();
+        Computer.Ram r1 = new Computer.Ram();
+
+        r1.getSpace();
+    }
+}
