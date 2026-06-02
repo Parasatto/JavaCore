530f75d963adb1c92ceadf77e76058401b80e1ab
PasikMacOs <frr.b006@gmail.com>
2026-06-02 17:57:40 +0500

    worked with serialaizble arrays

--- src/main/java/Serialaizble/ReadObject.java
+++ src/main/java/Serialaizble/ReadObject.java
@@ -3,16 +3,20 @@ package Serialaizble;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        FileInputStream fos = new FileInputStream("/Users/parasattynysbekov/Desktop/JavaSyntax/src/main/java/Serialaizble/savegame.bin");
+        FileInputStream fos = new FileInputStream("/Users/parasattynysbekov/Desktop/JavaSyntax/src/main/java/Serialaizble/all_heroes.bin");
         ObjectInputStream oos = new ObjectInputStream(fos);
 
-        GameCharacter players = (GameCharacter) oos.readObject();
-        System.out.println(players);
+
+
+        GameCharacter[] loadedCharacters = (GameCharacter[]) oos.readObject();
+
+        System.out.println(Arrays.toString(loadedCharacters));
 
         oos.close();
     }
--- src/main/java/Serialaizble/SaveGame.java
+++ src/main/java/Serialaizble/SaveGame.java
@@ -8,15 +8,25 @@ import java.io.ObjectOutputStream;
 public class SaveGame {
 
     public static void main(String[] args) throws IOException {
-
+/*
         GameCharacter player1 = new GameCharacter("Archer", 15, 80);
-        GameCharacter player2 = new GameCharacter("AshenOne", 18,65);
+        GameCharacter player2 = new GameCharacter("AshenOne", 18,65);*/
 
-        FileOutputStream fos = new FileOutputStream("/Users/parasattynysbekov/Desktop/JavaSyntax/src/main/java/Serialaizble/savegame.bin");
+        FileOutputStream fos = new FileOutputStream("/Users/parasattynysbekov/Desktop/JavaSyntax/src/main/java/Serialaizble/all_heroes.bin");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-
+/*
         oos.writeObject(player1);
-        oos.writeObject(player2);
+        oos.writeObject(player2);*/
+
+        GameCharacter[] characters = {
+                new GameCharacter("Parasat", 13, 100),
+                new GameCharacter("Azamat", 23, 99),
+                new GameCharacter("Saltanat", 24, 77)
+        };
+
+        oos.writeObject(characters);
+
+
 
         oos.close();
     }
