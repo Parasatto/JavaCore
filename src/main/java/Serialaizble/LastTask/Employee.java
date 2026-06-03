372d21585f4c0fa16633d9cf2f849b4a3990a7b6
PasikMacOs <frr.b006@gmail.com>
2026-06-03 13:50:05 +0500

    lask topik about serializable

---
+++ src/main/java/Serialaizble/LastTask/Employee.java
@@ -0,0 +1,49 @@
+package Serialaizble.LastTask;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private String position;
+    private int id;
+    private transient int salary;
+    private static final long serialVersionUID = 1L;
+
+    public Employee(String position, int id, int salary) {
+        this.position = position;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "position='" + position + '\'' +
+                ", id=" + id +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
