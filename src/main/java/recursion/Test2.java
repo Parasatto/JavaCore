ce4c43adb9b685c3e7a020a4cbc09773f708d943
PasikMacOs <frr.b006@gmail.com>
2026-06-05 23:33:45 +0500

    work with recursion

---
+++ src/main/java/recursion/Test2.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(sumDigis(123));
+    }
+
+    public static int sumDigis(int i){
+        if (i < 10)
+            return i;
+
+        return (i%10) + sumDigis(i /10);
+
+    }
+}
---
+++ src/main/java/recursion/Test3.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println(counter(5));
+    }
+
+    public static int counter(int i){
+        int j = 0;
+        if (i == j)
+            return i;
+        j++;
+        return counter(
+    }
+}
+/*
+A: От 1 до n
+Дано натуральное число n. Выведите все числа от 1 до n.
+ */
