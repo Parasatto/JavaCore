858d340650692a7628b50a36abb5f7268ebed3b1
PasikMacOs <frr.b006@gmail.com>
2026-07-09 17:22:41 +0500

    preperation for Junit

---
+++ src/main/java/Junit/MyMath.java
@@ -0,0 +1,15 @@
+package Junit;
+
+public class MyMath {
+
+    public static int add (int a, int b){
+        return a + b;
+    }
+
+    public static double divide(int a, int b) throws Exception {
+        if (a == 0 || b == 0){
+            throw new ArithmeticException("Can not be divided by zero");
+        }
+        return (double) a / b;
+    }
+}
