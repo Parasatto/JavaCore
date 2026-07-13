420cb94930217c87ccf817addb16a8c4ce58fea2
PasikMacOs <frr.b006@gmail.com>
2026-07-13 09:50:44 +0500

    creationg own anatation

--- src/main/java/Others/OldCalculator.java
+++ src/main/java/Others/OldCalculator.java
@@ -1,5 +1,10 @@
 package Others;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 public class OldCalculator {
 
     public static void main(String[] args) {
@@ -11,7 +16,20 @@ public class OldCalculator {
         return a + b;
     }
 
+
     public static int newSum(int a, int b){
         return a + b;
     }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    public @interface MethodInfo{
+        String author() default "Parasat";
+        int dateOfCreation();
+        String revision();
+
+    }
+
+    @MethodInfo(dateOfCreation = 2026, revision = "Test")
+    public static void test(){}
 }
