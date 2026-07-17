1295f097ac5a877b33fe803b05f53c747968128a
PasikMacOs <frr.b006@gmail.com>
2026-07-17 10:22:55 +0500

    upcasting&downcasting

--- src/main/java/Repeating/Lesson31/Main.java
+++ src/main/java/Repeating/Lesson31/Main.java
@@ -1,5 +1,29 @@
 package Repeating.Lesson31;
 
 public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        Vehicle myVehicle = myCar;
+        //myVehicle.refueil
 
+        Car myCar1 = (Car) myVehicle;
+        myCar1.refuel();
+
+
+        Bike myBike = new Bike();
+
+        Vehicle[] garage = {myBike, myCar};
+
+        for(Vehicle v: garage){
+            if(v instanceof Car){
+                Car myCary = (Car)v;
+                myCary.refuel();
+            } else if (v instanceof Bike) {
+                Bike MyBB = (Bike) v;
+                MyBB.ringBell();
+            }
+
+        }
+
+    }
 }
